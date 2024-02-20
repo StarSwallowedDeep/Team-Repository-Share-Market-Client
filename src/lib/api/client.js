1862e8c4981c262245578d11d235416e5b9bcb34
StarSwallowedDeep <139122242+StarSwallowedDeep@users.noreply.github.com>
2024-02-21 00:58:51 +0900

    Update client.js

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const client = axios.create();
-//const client = axios.create({
-//  baseURL: 'https://your-wep-server.app/',
-//});
+//const client = axios.create();
+const client = axios.create({
+  baseURL: 'https://port-0-server-17xco2nlsttmp5f.sel5.cloudtype.app/',
+});
 
 //client.defaults.headers.common['Authorization'] = 'Bearer (Token Information)';
 
